4c16c1b66553b64d4ed17f8bb93a374ced7d74d7
CarlosX26 <cjunior1@live.com>
2023-04-11 12:21:27 -0300

    feat: add component card presentation

--- src/components/CardPresentation/index.tsx
+++ src/components/CardPresentation/index.tsx
@@ -1,13 +1,23 @@
-import { Box, Button, Heading, Text } from "native-base"
+import { Box, Button, Heading, Stack, Text } from "native-base"
 
 export const CardPresentation = () => {
   return (
-    <Box bg="gray.100" p="16px" borderRadius="8px" flexDir="column" gap="16px">
-      <Heading>My Contacts</Heading>
-      <Text>Gerencie seus contatos de um jeito inovador.</Text>
-      <Button bg="cyan.600" fontWeight="bold">
-        Inscrever-se
-      </Button>
+    <Box bg="gray.100" p="16px" borderRadius="8px">
+      <Stack space="16px">
+        <Stack>
+          <Heading>My Contacts</Heading>
+        </Stack>
+        <Stack>
+          <Text>Gerencie seus contatos de um jeito inovador.</Text>
+        </Stack>
+        <Stack>
+          <Button bg="cyan.600">
+            <Text fontWeight="bold" textTransform="uppercase" color="gray.100">
+              Inscreva-se
+            </Text>
+          </Button>
+        </Stack>
+      </Stack>
     </Box>
   )
 }
