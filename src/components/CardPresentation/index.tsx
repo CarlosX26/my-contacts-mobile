73a3fa3fcbb37522d0749416b83492f9a1d590c4
CarlosX26 <cjunior1@live.com>
2023-04-11 12:32:44 -0300

    refactor: button

--- src/components/CardPresentation/index.tsx
+++ src/components/CardPresentation/index.tsx
@@ -4,19 +4,15 @@ export const CardPresentation = () => {
   return (
     <Box bg="gray.100" p="16px" borderRadius="8px">
       <Stack space="16px">
-        <Stack>
-          <Heading>My Contacts</Heading>
-        </Stack>
-        <Stack>
-          <Text>Gerencie seus contatos de um jeito inovador.</Text>
-        </Stack>
-        <Stack>
-          <Button bg="cyan.600">
-            <Text fontWeight="bold" textTransform="uppercase" color="gray.100">
-              Inscreva-se
-            </Text>
-          </Button>
-        </Stack>
+        <Heading>My Contacts</Heading>
+
+        <Text>Gerencie seus contatos de um jeito inovador.</Text>
+
+        <Button bg="cyan.600">
+          <Text fontWeight="bold" textTransform="uppercase" color="gray.100">
+            Inscreva-se
+          </Text>
+        </Button>
       </Stack>
     </Box>
   )
