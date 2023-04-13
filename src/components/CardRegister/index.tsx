6a99f176ad02973d556b6e9b974b91153b727b75
CarlosX26 <cjunior1@live.com>
2023-04-13 12:34:13 -0300

    feat: add method register

--- src/components/CardRegister/index.tsx
+++ src/components/CardRegister/index.tsx
@@ -5,12 +5,14 @@ import {
   Heading,
   Input,
   Link,
+  PresenceTransition,
   Stack,
   Text,
 } from "native-base"
 import { Controller, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
+import { useContactsContext } from "../../contexts/contacts"
 
 interface CardRegisterProps {
   toggleCard(card: string): void
@@ -47,9 +49,10 @@ const RegisterSchema = z.object({
     .regex(/(.{8,})|_/, "Mínimo de 8 caracteres."),
 })
 
-type Register = z.infer<typeof RegisterSchema>
+export type Register = z.infer<typeof RegisterSchema>
 
 export const CardRegister = ({ toggleCard }: CardRegisterProps) => {
+  const { register } = useContactsContext()
   const {
     control,
     handleSubmit,
@@ -59,126 +62,132 @@ export const CardRegister = ({ toggleCard }: CardRegisterProps) => {
   })
 
   const submit = (data: Register) => {
-    console.log(data)
+    register(data)
   }
 
   return (
-    <Box bg="gray.100" p="16px" borderRadius="8px">
-      <Stack space="16px">
-        <Heading>Cadastro</Heading>
-        <FormControl>
-          <Stack space="16px">
-            <Stack>
-              <FormControl.Label>Nome</FormControl.Label>
-              <Controller
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    variant="rounded"
-                    type="text"
-                    placeholder="Digite seu nome"
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name="name"
-              />
-            </Stack>
-            {errors.name && (
-              <Text color="red.400" fontSize="12px">
-                * {errors.name.message}
-              </Text>
-            )}
-            <Stack>
-              <FormControl.Label>Email</FormControl.Label>
-              <Controller
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    variant="rounded"
-                    type="text"
-                    placeholder="Digite seu email"
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name="email"
-              />
-            </Stack>
-            {errors.email && (
-              <Text color="red.400" fontSize="12px">
-                * {errors.email.message}
-              </Text>
-            )}
-            <Stack>
-              <FormControl.Label>Telefone</FormControl.Label>
-              <Controller
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    variant="rounded"
-                    type="text"
-                    placeholder="Digite seu número de telefone"
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name="phoneNumber"
-              />
-            </Stack>
-            {errors.phoneNumber && (
-              <Text color="red.400" fontSize="12px">
-                * {errors.phoneNumber.message}
-              </Text>
-            )}
-            <Stack>
-              <FormControl.Label>Senha</FormControl.Label>
-              <Controller
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <Input
-                    variant="rounded"
-                    type="password"
-                    placeholder="Digite sua senha"
-                    onChangeText={onChange}
-                    value={value}
-                  />
-                )}
-                name="password"
-              />
-            </Stack>
-            {errors.password && (
-              <Text color="red.400" fontSize="12px">
-                * {errors.password.message}
-              </Text>
-            )}
-
-            <Button
-              borderRadius="full"
-              bg="cyan.600"
-              onPress={handleSubmit(submit)}
-            >
-              <Text
-                fontWeight="bold"
-                textTransform="uppercase"
-                color="gray.100"
-              >
-                Cadastrar
-              </Text>
-            </Button>
-
-            <Box flexDir="row">
-              <Text>Já possui cadastro?</Text>
-
-              <Link onPress={() => toggleCard("login")}>
-                <Text color="cyan.600" pl="4px">
-                  login
+    <PresenceTransition
+      visible
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <Box bg="gray.100" p="16px" borderRadius="8px">
+        <Stack space="16px">
+          <Heading>Cadastro</Heading>
+          <FormControl>
+            <Stack space="16px">
+              <Stack>
+                <FormControl.Label>Nome</FormControl.Label>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      variant="rounded"
+                      type="text"
+                      placeholder="Digite seu nome"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                  name="name"
+                />
+              </Stack>
+              {errors.name && (
+                <Text color="red.400" fontSize="12px">
+                  * {errors.name.message}
                 </Text>
-              </Link>
-            </Box>
-          </Stack>
-        </FormControl>
-      </Stack>
-    </Box>
+              )}
+              <Stack>
+                <FormControl.Label>Email</FormControl.Label>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      variant="rounded"
+                      type="text"
+                      placeholder="Digite seu email"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                  name="email"
+                />
+              </Stack>
+              {errors.email && (
+                <Text color="red.400" fontSize="12px">
+                  * {errors.email.message}
+                </Text>
+              )}
+              <Stack>
+                <FormControl.Label>Telefone</FormControl.Label>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      variant="rounded"
+                      type="text"
+                      placeholder="Digite seu número de telefone"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                  name="phoneNumber"
+                />
+              </Stack>
+              {errors.phoneNumber && (
+                <Text color="red.400" fontSize="12px">
+                  * {errors.phoneNumber.message}
+                </Text>
+              )}
+              <Stack>
+                <FormControl.Label>Senha</FormControl.Label>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      variant="rounded"
+                      type="password"
+                      placeholder="Digite sua senha"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
+                  name="password"
+                />
+              </Stack>
+              {errors.password && (
+                <Text color="red.400" fontSize="12px">
+                  * {errors.password.message}
+                </Text>
+              )}
+
+              <Button
+                borderRadius="full"
+                bg="cyan.600"
+                onPress={handleSubmit(submit)}
+              >
+                <Text
+                  fontWeight="bold"
+                  textTransform="uppercase"
+                  color="gray.100"
+                >
+                  Cadastrar
+                </Text>
+              </Button>
+
+              <Box flexDir="row">
+                <Text>Já possui cadastro?</Text>
+
+                <Link onPress={() => toggleCard("login")}>
+                  <Text color="cyan.600" pl="4px">
+                    login
+                  </Text>
+                </Link>
+              </Box>
+            </Stack>
+          </FormControl>
+        </Stack>
+      </Box>
+    </PresenceTransition>
   )
 }
