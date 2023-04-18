59cc8b2fe036c14b5f5eea0524b37a26f63e65e8
CarlosX26 <cjunior1@live.com>
2023-04-17 21:32:04 -0300

    feat: add provider contacts

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,12 +1,15 @@
 import { View } from "native-base"
 import { ContactList } from "../../components/ContactList"
 import { NavBar } from "../../components/NavBar"
+import { ContactsProvider } from "../../contexts/contacts"
 
 export const Contacts = () => {
   return (
     <View h="100%">
-      <NavBar />
-      <ContactList />
+      <ContactsProvider>
+        <NavBar />
+        <ContactList />
+      </ContactsProvider>
     </View>
   )
 }
