6a3af01960cf9694470d32ea5e068a6d2ded3ac3
CarlosX26 <cjunior1@live.com>
2023-04-24 21:02:17 -0300

    feat: add pressable

--- src/components/CardContact/index.tsx
+++ src/components/CardContact/index.tsx
@@ -1,4 +1,13 @@
-import { Avatar, Box, Button, HStack, Heading, Text, Icon } from "native-base"
+import {
+  Avatar,
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Text,
+  Icon,
+  Pressable,
+} from "native-base"
 import { Feather } from "@expo/vector-icons"
 import { Contact, useContactsContext } from "../../contexts/contacts"
 
@@ -7,31 +16,50 @@ interface CardContactProps {
 }
 
 export const CardContact = ({ contact }: CardContactProps) => {
-  const { deleteContact } = useContactsContext()
+  const { deleteContact, selectContact } = useContactsContext()
 
   return (
-    <HStack bg="gray.100" space="4" position="relative" p="4" borderRadius="lg">
-      <Avatar>
-        {contact.fullName
-          .split(" ")
-          .map((e) => e.split("")[0])
-          .join("")}
-      </Avatar>
-      <Box>
-        <Heading>{contact.fullName}</Heading>
-        <Text>{contact.email}</Text>
-        <Text>{contact.phoneNumber}</Text>
-      </Box>
-      <Button
-        position="absolute"
-        top="2"
-        right="2"
-        bg="gray.200"
-        onPress={() => deleteContact(contact.id)}
-        _pressed={{ bg: "gray.300" }}
+    <Pressable
+      _pressed={{
+        style: {
+          transform: [
+            {
+              scale: 0.96,
+            },
+          ],
+        },
+      }}
+      onPress={() => selectContact(contact)}
+    >
+      <HStack
+        bg="gray.100"
+        space="4"
+        position="relative"
+        p="4"
+        borderRadius="lg"
       >
-        <Icon color="red.500" as={Feather} name="trash-2" />
-      </Button>
-    </HStack>
+        <Avatar>
+          {contact.fullName
+            .split(" ")
+            .map((e) => e.split("")[0])
+            .join("")}
+        </Avatar>
+        <Box>
+          <Heading>{contact.fullName}</Heading>
+          <Text>{contact.email}</Text>
+          <Text>{contact.phoneNumber}</Text>
+        </Box>
+        <Button
+          position="absolute"
+          top="2"
+          right="2"
+          bg="gray.200"
+          onPress={() => deleteContact(contact.id)}
+          _pressed={{ bg: "gray.300" }}
+        >
+          <Icon color="red.500" as={Feather} name="trash-2" />
+        </Button>
+      </HStack>
+    </Pressable>
   )
 }
