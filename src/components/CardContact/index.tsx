4606dc6e938cab9bdbfa71b5b70872abdf562a75
CarlosX26 <cjunior1@live.com>
2023-04-26 21:17:23 -0300

    feat: add altert dialog for delete contact

--- src/components/CardContact/index.tsx
+++ src/components/CardContact/index.tsx
@@ -7,9 +7,11 @@ import {
   Text,
   Icon,
   Pressable,
+  AlertDialog,
 } from "native-base"
 import { Feather } from "@expo/vector-icons"
 import { Contact, useContactsContext } from "../../contexts/contacts"
+import { useRef, useState } from "react"
 
 interface CardContactProps {
   contact: Contact
@@ -17,49 +19,91 @@ interface CardContactProps {
 
 export const CardContact = ({ contact }: CardContactProps) => {
   const { deleteContact, selectContact } = useContactsContext()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onClose = () => setIsOpen(false)
+
+  const cancelRef = useRef(null)
 
   return (
-    <Pressable
-      _pressed={{
-        style: {
-          transform: [
-            {
-              scale: 0.96,
-            },
-          ],
-        },
-      }}
-      onPress={() => selectContact(contact)}
-    >
-      <HStack
-        bg="gray.100"
-        space="4"
-        position="relative"
-        p="4"
-        borderRadius="lg"
+    <>
+      <Pressable
+        _pressed={{
+          style: {
+            transform: [
+              {
+                scale: 0.98,
+              },
+            ],
+          },
+        }}
+        onPress={() => selectContact(contact)}
       >
-        <Avatar>
-          {contact.fullName
-            .split(" ")
-            .map((e) => e.split("")[0])
-            .join("")}
-        </Avatar>
-        <Box>
-          <Heading>{contact.fullName}</Heading>
-          <Text>{contact.email}</Text>
-          <Text>{contact.phoneNumber}</Text>
-        </Box>
-        <Button
-          position="absolute"
-          top="2"
-          right="2"
-          bg="gray.200"
-          onPress={() => deleteContact(contact.id)}
-          _pressed={{ bg: "gray.300" }}
+        <HStack
+          bg="gray.100"
+          space="4"
+          position="relative"
+          p="4"
+          borderRadius="lg"
         >
-          <Icon color="red.500" as={Feather} name="trash-2" />
-        </Button>
-      </HStack>
-    </Pressable>
+          <Avatar>
+            {contact.fullName
+              .split(" ")
+              .map((e) => e.split("")[0])
+              .join("")}
+          </Avatar>
+          <Box>
+            <Heading>{contact.fullName}</Heading>
+            <Text>{contact.email}</Text>
+            <Text>{contact.phoneNumber}</Text>
+          </Box>
+          <Button
+            position="absolute"
+            top="2"
+            right="2"
+            bg="gray.200"
+            onPress={() => setIsOpen(true)}
+            _pressed={{ bg: "gray.300" }}
+          >
+            <Icon color="red.500" as={Feather} name="trash-2" />
+          </Button>
+        </HStack>
+      </Pressable>
+
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Excluir contato</AlertDialog.Header>
+          <AlertDialog.Body>
+            Você tem certeza de que deseja excluir este contato?
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={onClose}
+                ref={cancelRef}
+              >
+                Cancelar
+              </Button>
+              <Button
+                colorScheme="danger"
+                onPress={() => {
+                  onClose()
+                  deleteContact(contact.id)
+                }}
+              >
+                Confirmar
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
+    </>
   )
 }
