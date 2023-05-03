d6814becc7ebcabfb46888fa18176afb0db21d75
Carlos Junio <cjunior1@live.com>
2023-05-03 19:52:03 -0300

    feat: add validations of forms

--- src/validations/types.ts
+++ src/validations/types.ts
@@ -1,4 +1,16 @@
 import { z } from "zod"
 import { LoginForm } from "./loginForm"
+import { RegisterUserForm, UpdateUserForm } from "./userForm"
+import { RegisterContactForm, UpdateContactForm } from "./contactForm"
 
-export type Login = z.infer<typeof LoginForm>
+type Login = z.infer<typeof LoginForm>
+
+type RegisterUser = z.infer<typeof RegisterUserForm>
+
+type UpdateUser = z.infer<typeof UpdateUserForm>
+
+type RegisterContact = z.infer<typeof RegisterContactForm>
+
+type UpdateContact = z.infer<typeof UpdateContactForm>
+
+export { Login, RegisterUser, UpdateUser, RegisterContact, UpdateContact }
