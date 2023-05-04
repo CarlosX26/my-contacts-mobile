27303cb5b1a2cad216a4dd8a062db2f3b18cf237
Carlos Junio <cjunior1@live.com>
2023-05-03 21:42:21 -0300

    fix: provider

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -5,20 +5,17 @@ import { ContactsProvider } from "../../contexts/contacts"
 import { ModalProfile } from "../../components/ModalProfile"
 import { ModalContact } from "../../components/ModalContact"
 import { ModalSeeContact } from "../../components/ModalSeeContact"
-import { UserProvider } from "../../contexts/user"
 
 export const Contacts = () => {
   return (
     <View flex="1">
-      <UserProvider>
-        <ContactsProvider>
-          <NavBar />
-          <ContactList />
-          <ModalContact />
-          <ModalSeeContact />
-        </ContactsProvider>
-        <ModalProfile />
-      </UserProvider>
+      <ContactsProvider>
+        <NavBar />
+        <ContactList />
+        <ModalContact />
+        <ModalSeeContact />
+      </ContactsProvider>
+      <ModalProfile />
     </View>
   )
 }
